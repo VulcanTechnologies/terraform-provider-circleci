a9005e1f3ac7ddee0075a5131cf6314631e91a8f
StephenWithPH <StephenWithPH@users.noreply.github.com>
2020-10-09 16:03:36 -0700

    scaffold environment variable resource

---
+++ circleci/resource_environment_variable.go
@@ -0,0 +1,45 @@
+package circleci
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceEnvironmentVariable() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceEnvironmentVariableCreate,
+		ReadContext:   resourceEnvironmentVariableRead,
+		DeleteContext: resourceEnvironmentVariableDelete,
+		Schema: map[string]*schema.Schema{
+			"slug": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"value": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceEnvironmentVariableCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceEnvironmentVariableRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceEnvironmentVariableDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
---
+++ circleci/resource_environment_variable_test.go
@@ -0,0 +1,75 @@
+package circleci
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEnvironmentVariableResource(t *testing.T) {
+	testCases := map[string]func(*testing.T){
+		"resource argument for slug is named as expected": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			assert.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "slug")
+		},
+		"resource argument for slug is string": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			require.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "slug")
+			assert.Equal(t, schema.TypeString, Provider().ResourcesMap["circleci_environment_variable"].Schema["slug"].Type)
+		},
+		"resource argument for slug is required": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			require.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "slug")
+			assert.True(t, Provider().ResourcesMap["circleci_environment_variable"].Schema["slug"].Required)
+		},
+		"changing resource argument for slug forces new": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			require.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "slug")
+			assert.True(t, Provider().ResourcesMap["circleci_environment_variable"].Schema["slug"].ForceNew)
+		},
+		"resource argument for name is named as expected": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			assert.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "name")
+		},
+		"resource argument for name is string": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			require.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "name")
+			assert.Equal(t, schema.TypeString, Provider().ResourcesMap["circleci_environment_variable"].Schema["name"].Type)
+		},
+		"resource argument for name is required": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			require.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "name")
+			assert.True(t, Provider().ResourcesMap["circleci_environment_variable"].Schema["name"].Required)
+		},
+		"changing resource argument for name forces new": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			require.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "name")
+			assert.True(t, Provider().ResourcesMap["circleci_environment_variable"].Schema["name"].ForceNew)
+		},
+		"resource argument for value is named as expected": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			assert.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "value")
+		},
+		"resource argument for value is string": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			require.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "value")
+			assert.Equal(t, schema.TypeString, Provider().ResourcesMap["circleci_environment_variable"].Schema["value"].Type)
+		},
+		"resource argument for value is required": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			require.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "value")
+			assert.True(t, Provider().ResourcesMap["circleci_environment_variable"].Schema["value"].Required)
+		},
+		"changing resource argument for value forces new": func(t *testing.T) {
+			require.Contains(t, Provider().ResourcesMap, "circleci_environment_variable")
+			require.Contains(t, Provider().ResourcesMap["circleci_environment_variable"].Schema, "value")
+			assert.True(t, Provider().ResourcesMap["circleci_environment_variable"].Schema["value"].ForceNew)
+		},
+	}
+
+	for testCaseName, testCase := range testCases {
+		t.Run(testCaseName, testCase)
+	}
+}
